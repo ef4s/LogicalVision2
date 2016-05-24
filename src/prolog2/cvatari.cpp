db4ebff7dd72c6e65f66f6c2d5349849d46a1d85
Peter <ple15@ic.ac.uk>
2016-05-24 18:13:29 +0100

    First pass at cvatari files

---
+++ src/prolog2/cvatari.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peter on 24/05/16.
+//
+
+#include "cvatari.h"
---
+++ src/prolog2/cvatari.h
@@ -0,0 +1,14 @@
+//
+// Created by peter on 24/05/16.
+//
+
+#ifndef LOGICALVISION2_CVATARI_H
+#define LOGICALVISION2_CVATARI_H
+
+
+class cvatari {
+
+};
+
+
+#endif //LOGICALVISION2_CVATARI_H
